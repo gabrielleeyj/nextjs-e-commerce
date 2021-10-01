88f21e0ee44791dac36f7652c92c91461a85d34b
Gabriel <52558343+gabrielleeyj@users.noreply.github.com>
2021-10-01 11:50:45 +0800

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    BASE_URL: "http://localhost:3000",
+    // BASE_URL: "https://capstone-project-chi.vercel.app/",
+    MONGODB_URL:
+      "mongodb+srv://kannan:KA180990@e-commerce.gbb8r.mongodb.net/ecommerce?retryWrites=true&w=majority",
+    ACCESS_TOKEN_SECRET: "kannanazhagappa12332377947551327083",
+    REFRESH_TOKEN_SECRET: "kannanazhagappa123secret66018398rere45292697",
+    PAYPAL_CLIENT_ID:
+      "AW8RcHif_JR2RdNBInDIJpSO92r2SXgehRCiVgR-Muumi_sSdg8-qkRsH2QceFPa_F5hob4i7xa6YEUA",
+    CLOUD_UPDATE_PRESET: "ecommerce",
+    CLOUD_NAME: "kannanazhagappa",
+    CLOUD_API: "https://api.cloudinary.com/v1_1/kannanazhagappa/image/upload",
+  },
+};
+
+
