538930fac142620c14072f076a55d9a3b1786f98
Gabriel <52558343+gabrielleeyj@users.noreply.github.com>
2021-10-01 11:56:18 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 module.exports = {
   env: {
     BASE_URL: "http://localhost:3000",
-    // BASE_URL: "https://capstone-project-chi.vercel.app/",
+    // BASE_URL: process.env.BASE_URL,
     MONGODB_URL:
-      "mongodb+srv://kannan:KA180990@e-commerce.gbb8r.mongodb.net/ecommerce?retryWrites=true&w=majority",
-    ACCESS_TOKEN_SECRET: "kannanazhagappa12332377947551327083",
-    REFRESH_TOKEN_SECRET: "kannanazhagappa123secret66018398rere45292697",
+      process.env.MONGODB_URL,
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN,
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN,
     PAYPAL_CLIENT_ID:
-      "AW8RcHif_JR2RdNBInDIJpSO92r2SXgehRCiVgR-Muumi_sSdg8-qkRsH2QceFPa_F5hob4i7xa6YEUA",
-    CLOUD_UPDATE_PRESET: "ecommerce",
-    CLOUD_NAME: "kannanazhagappa",
-    CLOUD_API: "https://api.cloudinary.com/v1_1/kannanazhagappa/image/upload",
+      process.env.PAYPAL_CLIENT_ID,
+    CLOUD_UPDATE_PRESET: process.env.CLOUD_UPDATE_PRESET,
+    CLOUD_NAME: process.env.CLOUD_NAME,
+    CLOUD_API: process.env.CLOUD_API,
   },
 };
 
